c64664c6d7f81f1a3e2be35b4f11e88befc6811a
Fabrizio Feitosa <fabriziofeitosa@live.com>
2019-11-06 22:20:45 -0300

    Login e seus modals concluidos

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,21 +14,6 @@ jQuery(document).ready(function ($) {
 
     // ################################################
 
-    // Slide Home
-    $('.pag-home .slide-home').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        fade: true,
-        cssEase: 'linear'
-    });
-
-    // ################################################
-
     // Animação input buscar
     let header = $('.pag-home header');
     $('.pag-home header #inputBuscar').focusin(function () {
@@ -49,4 +34,56 @@ jQuery(document).ready(function ($) {
         $('.btn-principal').toggleClass('aberto');
     });
 
+    // ################################################
+
+    /*
+    * Replace all SVG images with inline SVG
+    */
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+    });
+
+    // ################################################
+
+    // Slide Home
+    $('.pag-home .slide-home').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
